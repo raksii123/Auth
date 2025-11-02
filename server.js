8150382047715ca6e09be9fea791dc69f8abafeb
Rakshith. G .Hiremath <rakshithghiremath028@gmail.com>
2025-11-02 13:34:26 +0530

    first commit

--- server.js
+++ server.js
@@ -13,8 +13,3 @@ app.listen(3001, () => {
 
 
 
-// MONGO_URI=mongodb://localhost:27017/spotify-auth
-// JWT_SECRET=595d55faadb14c502768aba283db6d6d
-// CLIENT_ID=966860407497-2gijj8gjsncqr5afnlgn0r02fnsnjl5t.apps.googleusercontent.com
-// CLIENT_SECRET=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
-// RABBITMQ_URI=amqps://yozdwlbb:WaZsolAPXI8VPCKpi6pIzMsWCBcxdgIj@fuji.lmq.cloudamqp.com/yozdwlbb
